b27f5e3fa1761814d8d2574e9bb7f0f0f9606543
Sumit Khandelwal <synergy2411@outlook.com>
2022-11-28 15:57:10 +0530

    product edit

---
+++ src/Components/Products/ProductEdit/ProductEdit.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import classes from './ProductEdit.module.css';
+
+const ProductEdit = () => {
+    return (
+        <div className={classes["my-backdrop"]}>
+            <div className={classes["alert-dialog"]}>
+                ProductEdit coming soon...
+            </div>
+        </div>
+    );
+}
+
+export default ProductEdit;
--- src/Components/Products/ProductList/ProductList.js
+++ src/Components/Products/ProductList/ProductList.js
@@ -1,8 +1,10 @@
 import { useEffect, useState } from "react";
+import ProductEdit from "../ProductEdit/ProductEdit";
 
 const ProductList = () => {
 
     const [products, setProducts] = useState([]);
+    const [showEdit, setShowEdit] = useState(false);
 
     useEffect(() => {
         const fetchProducts = async () => {
@@ -13,6 +15,9 @@ const ProductList = () => {
         fetchProducts()
     }, [])
 
+
+    const deleteClickHandler = () => setShowEdit(true)
+
     return (
         <div className="container">
             <table className="table table-hover">
@@ -22,6 +27,7 @@ const ProductList = () => {
                         <th>Product Name</th>
                         <th>Company</th>
                         <th>Price</th>
+                        <th>Action</th>
                     </tr>
                 </thead>
                 <tbody>
@@ -31,11 +37,13 @@ const ProductList = () => {
                             <td>{product.name.toUpperCase()}</td>
                             <td>{product.company.toUpperCase()}</td>
                             <td>${product.price}</td>
+                            <td><button onClick={deleteClickHandler} className="btn btn-outline-danger btn-sm">DELETE</button></td>
                         </tr>
                     })}
 
                 </tbody>
             </table>
+            {showEdit && <ProductEdit />}
         </div>
     )
 }
